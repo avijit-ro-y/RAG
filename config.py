51652ce95977d2997eb23d954d0b70b1c3c4f1a8
Avijit <avijitroy926avijit@gmail.com>
2026-06-07 03:41:45 +0530

    textprocessor file done

--- config.py
+++ config.py
@@ -26,3 +26,7 @@ LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s" #It defines 
 PROJECT_ROOT = Path(__file__).parent #__file__ means current file path (Example project/config.py )...Path(__file__) converts string → Path object....parent gets folder
 LOG_DIR = PROJECT_ROOT / "logs" #Combines path (project/ + logs)...Result project/logs/
 DATA_DIR = PROJECT_ROOT / "data"
+
+#Text Processing
+CHUNK_SIZE = 1000
+CHUNK_OVERLAP = 200
